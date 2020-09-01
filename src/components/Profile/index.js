7671a367c4df916817fb3eb284c0e05240ae8fd5
Kelvin Oliveira <keelviinn@gmail.com>
2020-08-31 22:22:14 -0300

    images config and plugin

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Avatar from '../Avatar'
+
 export default function Profile() {
   const { 
     site: {
@@ -26,6 +28,7 @@ export default function Profile() {
 
   return (
     <div className="Profile-wrapper">
+      <Avatar />
       <h1>{title}</h1>
       <h2>{position}</h2>
       <p>{description}</p>
