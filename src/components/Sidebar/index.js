ddcd6fa1d8160478de895dae7743777700168e1b
Kelvin Oliveira <keelviinn@gmail.com>
2020-09-01 18:58:32 -0300

    Social Links Container

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Profile from '../Profile'
+import Profile from '../Profile';
+import SocialLinks from '../SocialLinks';
 
 import { SidebarWrapper } from './styled';
 
 const Sidebar = () => (
   <SidebarWrapper>
     <Profile />
+    <SocialLinks />
   </SidebarWrapper>  
 )
 
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/keelviinn'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/k_henriqqe'
+  },
+  {
+    label: 'Youtube',
+    url: 'https://www.youtube.com/channel/UCb1dHjTAPo1adyDYH0mU8FA'
+  },
+  {
+    label: 'Instagram',
+    url: 'https://www.instagram.com/k_henriqqe'
+  }
+]
+
+export default links;
