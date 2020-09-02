97defb0aeae97099414c84bffe57f5e9d2084193
Kelvin Oliveira <keelviinn@gmail.com>
2020-09-02 19:43:30 -0300

    adding style at posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
+
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
+  
   return (
-    <>
-      <div>
+    <Layout>
+        <SEO title={post.frontmatter.title} />
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
-    </>
+    </Layout>
   )
 }
 
@@ -17,8 +20,11 @@ export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      timeToRead
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
     }
   }
