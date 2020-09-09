f3b6d29db887b76d631b5897e65b503c0e616784
Kelvin Oliveira <keelviinn@gmail.com>
2020-09-08 22:15:45 -0300

    styled css grid at post list when in grid layout

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from 'react'
+import { graphql } from 'gatsby'
 
-import PostItem from '../components/PostItem';
-import Pagination from '../components/Pagination';
-import Layout from '../components/Layout';
-import SEO from '../components/seo';
+import PostItem from '../components/PostItem'
+import Pagination from '../components/Pagination'
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+
+import { ListWeapper } from '../components/ListWrapper/styles'
 
 export default function BlogList(props) {
   const postList = props.data.allMarkdownRemark.edges
@@ -12,46 +14,42 @@ export default function BlogList(props) {
   const { currentPage, numPages } = props.pageContext
   const isFirst = currentPage === 1
   const isLast = currentPage === numPages
-  const prevPage = (currentPage - 1 === 1 ? "/" : (currentPage - 1)).toString()
+  const prevPage = (currentPage - 1 === 1 ? '/' : currentPage - 1).toString()
   const nextPage = (currentPage + 1).toString()
-  
+
   return (
     <Layout>
       <SEO title="Home" />
-      {postList.map(({ 
-        node: {
-          frontmatter: {
-            background,
-            category,
-            date,
-            description,
-            title
-          },
-          timeToRead,
-          fields: {
-            slug
-          }
-        }, 
-      }) => (
-        <PostItem
-        slug={slug}
-        background={background}
-        category={category}
-        date={date}
-        timeToRead={timeToRead}
-        title={title}
-        description={description}
-      />
-      ))}  
+      <ListWeapper>
+        {postList.map(
+          ({
+            node: {
+              frontmatter: { background, category, date, description, title },
+              timeToRead,
+              fields: { slug },
+            },
+          }) => (
+            <PostItem
+              slug={slug}
+              background={background}
+              category={category}
+              date={date}
+              timeToRead={timeToRead}
+              title={title}
+              description={description}
+            />
+          )
+        )}
+      </ListWeapper>
 
-      <Pagination 
-        isFirst={isFirst} 
+      <Pagination
+        isFirst={isFirst}
         isLast={isLast}
         currentPage={currentPage}
         numPages={numPages}
         prevPage={prevPage}
         nextPage={nextPage}
-      />    
+      />
     </Layout>
   )
 }
@@ -62,7 +60,7 @@ export const query = graphql`
       sort: { fields: [frontmatter___date], order: DESC }
       limit: $limit
       skip: $skip
-      ) {
+    ) {
       edges {
         node {
           fields {
